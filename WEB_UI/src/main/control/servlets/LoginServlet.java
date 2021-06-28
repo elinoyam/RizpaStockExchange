6ba3421c0d2a9c8d72dbf7bfb6dedf6586968cc5
guypodolsky <guypo@mta.ac.il>
2021-06-28 22:05:23 +0300

    Login page updated

--- WEB_UI/src/main/control/servlets/LoginServlet.java
+++ WEB_UI/src/main/control/servlets/LoginServlet.java
@@ -3,7 +3,7 @@ package main.control.servlets;
 import com.sun.javaws.exceptions.InvalidArgumentException;
 import users.UsersManager;
 
-import javax.servlet.annotation.WebServlet;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,7 +11,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.http.HttpSession;
 
-@WebServlet (name = "LoginServlet", urlPatterns ={"/login"})
+
 public class LoginServlet extends HttpServlet {
 
     private final String Dash_Board_URL = "../../webapp/pages/dashboard/dashboard.html"; //page no. 2
@@ -57,14 +57,39 @@ public class LoginServlet extends HttpServlet {
 
     }
 
-        /**
-         * Handles the HTTP <code>GET</code> method.
-         *
-         * @param request servlet request
-         * @param response servlet response
-         */
-        @Override
-        protected void doGet(HttpServletRequest request, HttpServletResponse response){
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response){
             login(request, response);
         }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        login(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
 }
